bca2fe77b3c5e433524c3c7283ab8261b4c57b89
KindEni <kind_official@outlook.com>
2023-02-15 06:30:01 +0100

    added toggle button for theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,32 @@ import { ColorSliders } from './ColorSliders';
 import { toRGB } from './utilities';
 import { reducer } from './reducer';
 import { ThemeContext } from './theme-context';
+import { useState } from 'react';
 
 import { ColorAdjustment } from './colorAdjustment';
 import { ColorSlider } from './ColorSlider';
 import { ColorInput } from './ColorInput';
 
+
+
 const App = () => {
   const themes = React.useContext(ThemeContext);
+
+  const [theme, toggleTheme] = useState(true)
 
   const [rgb, dispatch] = React.useReducer(reducer, {
     red: 0,
     green: 0,
     blue: 0,
   });
-
   return (
-    <main style={{ borderColor: toRGB(rgb), ...themes.light }}>
+    <main style={theme ? { borderColor: toRGB(rgb), ...themes.light}: { borderColor: toRGB(rgb), ...themes.dark}}>
       <ColorSwatch />
       {/* <ColorInputs {...rgb} /> */}
       {/* <ColorSliders/> */}
       <ColorAdjustment Adjustment={ColorSlider}/>
       <ColorAdjustment Adjustment={ColorInput}/>
+      <button  onClick={() => toggleTheme(!theme)} className="btn">toggle theme</button>
     </main>
   );
 };
